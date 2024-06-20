f8394280083811e9522875a79aba8723b5d27709
Rossana Liendo <rossana.liendo@gmail.com>
2024-06-20 10:31:40 +0200

    Fixed Kata 39

--- kata39.php
+++ kata39.php
@@ -16,34 +16,40 @@ $arrayText = explode(" ", $contenido);
 
 // Split the string into an array of words using space as the delimiter
 //$arrayText = preg_split('/\s+/', $contenido);
-function findWordsWithVowel($arrayText): array {
+function startsWithVowel($arrayText): array {
     $wordsWithVowel = [];
     $vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
     foreach($arrayText as $word) {
-        $hasVowel = false;
-        for($i = 0; $i < strlen($word); $i++) {
-            if (in_array($word[$i], $vowels)) {
-                $hasVowel = true;
+        if(in_array($word[0], $vowels)){
+            $wordsWithVowel[] = $word;
             }
         }
-        if($hasVowel) {
-            $wordsWithVowel[] = $word;
-        }
+        return $wordsWithVowel;
     }
-    return $wordsWithVowel;
-}
-function findWordsStartingWithT($arrayText): array {
+
+function containsT($arrayText): array {
     $wordsWithT = [];
-    $letter = ["t", "T"];
+
+/*  $letter = ["t", "T"];
     foreach ($arrayText as $word) {
-        if (in_array($word[0], $letter)) {
+        for($i = 0; $i < strlen($word); $i++)
+            if (in_array($word[$i], $letter)) {
+                $wordsWithT[] = $word;
+                break;
+        }
+    } */
+
+    //Factorizado
+    foreach ($arrayText as $word) {
+        if (stripos($word, "t") !== false) {
             $wordsWithT[] = $word;
         }
     }
     return $wordsWithT;
 }
 
-print_r(findWordsWithVowel($arrayText));
-print_r(findWordsStartingWithT($arrayText));
+print_r(startsWithVowel($arrayText));
+print_r(containsT($arrayText));
 
 fclose($gestor);
