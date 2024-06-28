52e81a3ce81898acf4267751dc57b8149b3fd7ee
Rossana Liendo <rossana.liendo@gmail.com>
2024-06-28 11:43:47 +0200

    Finished the kata

--- kata41.php
+++ kata41.php
@@ -3,59 +3,109 @@
 $date1 = '15/05/2024';
 $date2 = '15/06/2024';
 $date3 = '45/15/2024';
+//Leap year
+$date4 = '29/02/2024';
 
-function verifyDate(string $date): string
-{
+function verifyDate(string $date): bool
+{   
+    $formatIsCorrect = true;
+    //Convert to 3 differente variables
     $separator = '/';
-
     $dateParts = explode($separator, $date);
 
     $day = (int)$dateParts[0];
     $month = (int)$dateParts[1];
     $year = (int)$dateParts[2];
 
-    $months31Days = [1, 3, 5, 7, 8, 10, 12];
-    $month30Days = [4, 6, 9, 11];
+    if (count($dateParts) !== 3) {
+        $formatIsCorrect = false;
+    }
 
-    $message = "Format is correct.";
+    if($day > 32 || $month > 12) {
+        $formatIsCorrect = false;
+    }
 
-    $leapYear = false;
-
-
-    if($year % 4 = 0){
+    //Verify if it's leap year
+    $isLeapYear = false;
+    if($year % 4 === 0 && ($year % 100 !== 0 || $year % 400 === 0)) {
+        $isLeapYear = true;
         if($month > 29) {
-            $message = "Error";
+            $formatIsCorrect = false;
         }
     }
-
-    if(in_array($month, $month30Days) && $day > 30) {
-        $message = "Error" . PHP_EOL;
-    } else if (in_array($month, $months31Days) && $day > 31) {
-        $message = "Error" . PHP_EOL;
-    } else if ($month = 02 && $day > 28){
-        $message = "Error" . PHP_EOL;
+    //Verify for non-leap years
+    $months31Days = [1, 3, 5, 7, 8, 10, 12];
+    $month30Days = [4, 6, 9, 11];
+    if($isLeapYear = false) {
+        if(in_array($month, $month30Days) && $day > 30) {
+            $formatIsCorrect = false;
+        } else if (in_array($month, $months31Days) && $day > 31) {
+            $formatIsCorrect = false;
+        } else if ($month = 02 && $day > 28) {
+            $formatIsCorrect = false;
+        }
     }
-    return $message;
+    return $formatIsCorrect;
 }
 
-function remainingDays(string $date1, string $date2)
+function remainingDays(string $date1, string $date2): string
 {
-    $format = "dd/mm/YYYY";
-
+    // Verify if dates are in the correct format
+    if (!verifyDate($date1) || !verifyDate($date2)) {
+        return "Date format is incorrect." . PHP_EOL;
+    }
+    // Change dates to proper format
+    $format = 'd/m/Y';
     $date1 = DateTime::createFromFormat($format, $date1);
     $date2 = DateTime::createFromFormat($format, $date2);
-
+    // Check if date creation was successful
+    if ($date1 === false || $date2 === false) {
+        return "Date format is incorrect." . PHP_EOL;
+    }
+    // Get interval of days
     $interval = $date1->diff($date2);
-
-    return $interval->days . PHP_EOL;
-
+    return "There are " . $interval->days . " days of difference." . PHP_EOL;
 }
 
-
-echo verifyDate($date1);
-echo verifyDate($date3);
-
 echo remainingDays($date1, $date2);
-
 echo remainingDays($date1, $date3);
+echo remainingDays($date2, $date4);
+
+//Luego descubrí que existe la función checkdate
+function verifyDate2(string $date): bool
+{   
+    $separator = '/';
+    $dateParts = explode($separator, $date);
+
+    $day = (int)$dateParts[0];
+    $month = (int)$dateParts[1];
+    $year = (int)$dateParts[2];
+
+    if (!checkdate($month, $day, $year)) {
+        return false;
+    }
+    return true;
+}
+
+function remainingDays2(string $date1, string $date2): string
+{
+    // Verify if dates are in the correct format
+    if (!verifyDate2($date1) || !verifyDate2($date2)) {
+        return "Date format is incorrect." . PHP_EOL;
+    }
+    // Change dates to proper format
+    $format = 'd/m/Y';
+    $date1 = DateTime::createFromFormat($format, $date1);
+    $date2 = DateTime::createFromFormat($format, $date2);
+    // Check if date creation was successful
+    if ($date1 === false || $date2 === false) {
+        return "Date format is incorrect." . PHP_EOL;
+    }
+    // Get interval of days
+    $interval = $date1->diff($date2);
+    return "There are " . $interval->days . " days of difference." . PHP_EOL;
+}
+
+echo remainingDays2($date1, $date4);
+echo remainingDays2($date1, $date3);
 
