e4f30c04a8e5c7fedbfe50539a660d639028bfe8
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-11 13:16:21 +0200

    Added notes

--- kata42.php
+++ kata42.php
@@ -3,9 +3,6 @@
 // Set the internal character encoding to UTF-8
 mb_internal_encoding("UTF-8");
 
-// Set the output encoding to UTF-8
-mb_http_output("UTF-8");
-
 echo "Enter a phrase, please: ";
 
 $text = trim(fgets(STDIN));
@@ -15,6 +12,7 @@ $array = explode(' ', $text);
 $longestWord = ''; 
 
 //Find the longest word
+//md reads special characters as one character
 foreach($array as $word) {
     if(mb_strlen($word) > mb_strlen($longestWord)) {
         $longestWord = $word;
@@ -30,11 +28,9 @@ echo $borderLine . PHP_EOL;
 for($i = 0; $i < count($array); $i++) {
 
     $wordLength = mb_strlen($array[$i]);
-
     $spaces = $longestLength - $wordLength;
 
     $leftSpaces = str_repeat(' ', (int) intdiv($spaces, 2));
-
     $rightSpaces = str_repeat(' ',  (int)  $spaces - intdiv($spaces, 2));
 
     echo '# ' . $leftSpaces . $array[$i] . $rightSpaces . ' #' . PHP_EOL;
