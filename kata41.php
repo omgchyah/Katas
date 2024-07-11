6aa4bddc53725b4fcdbfe9089c27be65c4e18b0d
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-11 13:13:38 +0200

    Corrected Kata 41

--- kata41.php
+++ kata41.php
@@ -2,21 +2,56 @@
 
 $date1 = '15/05/2024';
 $date2 = '15/06/2024';
+//Incorrect date
 $date3 = '45/15/2024';
 //Leap year
 $date4 = '29/02/2024';
 
 function verifyDate(string $date): bool
 {   
-    $formatIsCorrect = true;
-    //Convert to 3 differente variables
-    $separator = '/';
-    $dateParts = explode($separator, $date);
+    $dateParts = explode('/', $date);
 
     $day = (int)$dateParts[0];
     $month = (int)$dateParts[1];
     $year = (int)$dateParts[2];
 
+    return checkdate($month, $day, $year);
+}
+
+function remainingDays(string $date1, string $date2): string
+{
+    // Verify if dates are in the correct format
+    if (!verifyDate($date1) || !verifyDate($date2)) {
+        return "Date format is incorrect." . PHP_EOL;
+    }
+    // Change dates to proper format
+    $format = 'd/m/Y';
+    $date1 = DateTime::createFromFormat($format, $date1);
+    $date2 = DateTime::createFromFormat($format, $date2);
+
+    // Get interval of days
+    $interval = $date1->diff($date2);
+    return "There are " . $interval->days . " days of difference." . PHP_EOL;
+}
+
+echo remainingDays($date1, $date4);
+echo remainingDays($date1, $date3);
+
+
+
+
+/* function verifyDate(string $date): bool
+{   
+    $formatIsCorrect = true;
+
+    //Convert to 3 differente variables
+    $dateParts = explode('/', $date);
+
+    $day = (int)$dateParts[0];
+    $month = (int)$dateParts[1];
+    $year = (int)$dateParts[2];
+
+    //Check that number of elements is 3
     if (count($dateParts) !== 3) {
         $formatIsCorrect = false;
     }
@@ -25,18 +60,13 @@ function verifyDate(string $date): bool
         $formatIsCorrect = false;
     }
 
-    //Verify if it's leap year
-    $isLeapYear = false;
     if($year % 4 === 0 && ($year % 100 !== 0 || $year % 400 === 0)) {
-        $isLeapYear = true;
         if($month > 29) {
             $formatIsCorrect = false;
         }
-    }
-    //Verification for non-leap years
-    $months31Days = [1, 3, 5, 7, 8, 10, 12];
-    $month30Days = [4, 6, 9, 11];
-    if($isLeapYear = false) {
+    } else {
+        $months31Days = [1, 3, 5, 7, 8, 10, 12];
+        $month30Days = [4, 6, 9, 11];
         if(in_array($month, $month30Days) && $day > 30) {
             $formatIsCorrect = false;
         } else if (in_array($month, $months31Days) && $day > 31) {
@@ -45,10 +75,12 @@ function verifyDate(string $date): bool
             $formatIsCorrect = false;
         }
     }
+
     return $formatIsCorrect;
 }
 
-function remainingDays(string $date1, string $date2): string
+ */
+/* function remainingDays(string $date1, string $date2): string
 {
     // Verify if dates are in the correct format
     if (!verifyDate($date1) || !verifyDate($date2)) {
@@ -69,43 +101,7 @@ function remainingDays(string $date1, string $date2): string
 
 echo remainingDays($date1, $date2);
 echo remainingDays($date1, $date3);
-echo remainingDays($date2, $date4);
+echo remainingDays($date2, $date4); */
 
-//Luego descubrí que existe la función checkdate
-function verifyDate2(string $date): bool
-{   
-    $separator = '/';
-    $dateParts = explode($separator, $date);
 
-    $day = (int)$dateParts[0];
-    $month = (int)$dateParts[1];
-    $year = (int)$dateParts[2];
-
-    if (!checkdate($month, $day, $year)) {
-        return false;
-    }
-    return true;
-}
-
-function remainingDays2(string $date1, string $date2): string
-{
-    // Verify if dates are in the correct format
-    if (!verifyDate2($date1) || !verifyDate2($date2)) {
-        return "Date format is incorrect." . PHP_EOL;
-    }
-    // Change dates to proper format
-    $format = 'd/m/Y';
-    $date1 = DateTime::createFromFormat($format, $date1);
-    $date2 = DateTime::createFromFormat($format, $date2);
-    // Check if date creation was successful
-    if ($date1 === false || $date2 === false) {
-        return "Date format is incorrect." . PHP_EOL;
-    }
-    // Get interval of days
-    $interval = $date1->diff($date2);
-    return "There are " . $interval->days . " days of difference." . PHP_EOL;
-}
-
-echo remainingDays2($date1, $date4);
-echo remainingDays2($date1, $date3);
 
--- kata42.php
+++ kata42.php
@@ -1,32 +1,44 @@
 <?php declare(strict_types=1);
 
+// Set the internal character encoding to UTF-8
+mb_internal_encoding("UTF-8");
+
+// Set the output encoding to UTF-8
+mb_http_output("UTF-8");
+
 echo "Enter a phrase, please: ";
 
 $text = trim(fgets(STDIN));
 
 $array = explode(' ', $text);
 
+$longestWord = ''; 
+
+//Find the longest word
 foreach($array as $word) {
-   $longestWord = ''; 
-    if(strlen($word) > strlen($longestWord)) {
+    if(mb_strlen($word) > mb_strlen($longestWord)) {
         $longestWord = $word;
     }
 }
 
-$longestLength = strlen($longestWord);
+$longestLength = mb_strlen($longestWord);
 
 $borderLine = str_repeat('#', $longestLength + 4);
 
 echo $borderLine . PHP_EOL;
 
 for($i = 0; $i < count($array); $i++) {
-    $wordLength = strlen(($array[$i]));
+
+    $wordLength = mb_strlen($array[$i]);
+
     $spaces = $longestLength - $wordLength;
 
-    $leftSpaces = str_repeat(' ', intdiv($spaces, 2));
-    $rightSpaces = str_repeat(' ', $spaces - intdiv($spaces, 2));
+    $leftSpaces = str_repeat(' ', (int) intdiv($spaces, 2));
+
+    $rightSpaces = str_repeat(' ',  (int)  $spaces - intdiv($spaces, 2));
 
     echo '# ' . $leftSpaces . $array[$i] . $rightSpaces . ' #' . PHP_EOL;
+
 }
 
 echo $borderLine;
