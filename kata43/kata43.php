3d038ee3cd7f98db88a468d46a33b049160f84bb
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-29 10:37:55 +0200

    Working with a do-while and switch

--- kata43/kata43.php
+++ kata43/kata43.php
@@ -3,80 +3,111 @@
 require 'StudyPlan.php';
 require 'VacationPlan.php';
 
-echo "Hola. Por favor, indique si desea guardar un plan de estudios o un plan de vacaciones ('E' o 'V'): " . PHP_EOL;
+$option = 0;
 
-$planType = strtoupper(trim(readline()));
+do {
 
-if ($planType === 'E') {
-    $plan = new StudyPlan();
-} elseif ($planType === 'V') {
-    $plan = new VacationPlan();
-} else {
-    echo "Tipo de plan no válido. Use 'E' para plan de estudios o 'V' para plan de vacaciones." . PHP_EOL;
-    exit;
-}
+    echo "¡Hola! Por favor, seleccione una opción." . PHP_EOL .
+    "1. Crear plan vacacional." . PHP_EOL .
+    "2. Anular plan vacacional." . PHP_EOL .
+    "3. Planificar entrega IT Academy." . PHP_EOL .
+    "4. Planificar reentrega IT Academy." . PHP_EOL .
+    "5. Revisar agenda de día." . PHP_EOL .
+    "0. Salir." . PHP_EOL .
 
-echo 'Por favor, ingrese la fecha de su plan en formato (dd-mm-aaaa): ' . PHP_EOL;
-$dateInput = trim(readline());
-$date = DateTime::createFromFormat('d-m-Y', $dateInput);
+    $option = (int)(readline());
 
-if ($date === false) {
-    throw new Exception("Formato de fecha no válido. Use (dd-mm-aaaa)." . PHP_EOL);
-}
-
-$plan->setDate($date);
-
-if (!$plan->isDateAvalaible($date)) {
-    echo 'Lo siento. Fecha no disponible.' . PHP_EOL;
-    exit;
-}
-
-if ($planType === 'E') {
-    echo 'Ingrese el nombre del plan: ' . PHP_EOL;
-    $name = trim(readline());
-    $plan->setName($name);
-
-    echo 'Ingrese el número de Sprint (1-5): ' . PHP_EOL;
-    $sprintNumber = (int)trim(readline());
-
-    if (!in_array($sprintNumber, range(1, 5))) {
-        throw new Exception("Número de Sprint no válido. Use un número entre 1 y 5." . PHP_EOL);
-    }
-
-    $sprint = Sprint::from($sprintNumber);
-    $plan->setSprint($sprint);
-
-    echo 'Ingrese el enlace de GitHub: ' . PHP_EOL;
-    $gitHubLink = trim(readline());
-    $plan->setGitHubLink($gitHubLink);
-
-    echo 'Si lo desea, agregué alguna nota: ' . PHP_EOL;
-    $notes = trim(readline());
-    $plan->setNotes($notes);
-
-} else if ($planType === 'V') {
-    echo 'Ingrese el nombre del plan: ' . PHP_EOL;
-    $name = trim(readline());
-    $plan->setName($name);
-
-    echo 'Ingrese la ubicación del plan: ' . PHP_EOL;
-    $location = trim(readline());
-    $plan->setLocation($location);
-
-    echo 'Ingrese el tipo del plan (Restaurant, Sports, Cultural, Visit): ' . PHP_EOL;
-    $typeInput = ucfirst(trim(readline())); 
-
-    if (!in_array($typeInput, array_column(Type::cases(), 'value'))) {
-        throw new Exception("Tipo de plan no válido. Use 'Restaurant', 'Sports', 'Cultural', o 'Visit'." . PHP_EOL);
+    switch($option) {
+        case 1 & 3:
+            echo 
     }
 
 
-    $type = Type::from($typeInput);
-    $plan->setType($type);
+
+} while ($opcion != 0);
+
+
+function checkDate(string $dateInput)
+{
+    $date = DateTime::createFromFormat('d-m-Y', $dateInput);
+
+    if ($date === false) {
+        throw new Exception("Formato de fecha no válido. Use (dd-mm-aaaa)." . PHP_EOL);
+    }
+
+    $plan->setDate($date);
+
+    if (!$plan->isDateAvalaible($date)) {
+        echo 'Lo siento. Fecha no disponible.' . PHP_EOL;
+        exit;
+    }
 }
 
-if ($plan->create()) {
-    echo "¡Plan creado exitosamente!" . PHP_EOL;
-} else {
-    echo "Error al crear el plan." . PHP_EOL;
+function createVacationPlan(int $opcion)
+{
+    echo 'Por favor, ingrese la fecha de su plan en formato (dd-mm-aaaa): ' . PHP_EOL;
+    $dateInput = trim(readline());
+
+    checkDate($dateInput);
+
+    echo 'Ingrese el nombre del plan: ' . PHP_EOL;
+        $name = trim(readline());
+        $plan->setName($name);
+
+        echo 'Ingrese el número de Sprint (1-5): ' . PHP_EOL;
+        $sprintNumber = (int)trim(readline());
+
+        if (!in_array($sprintNumber, range(1, 5))) {
+            throw new Exception("Número de Sprint no válido. Use un número entre 1 y 5." . PHP_EOL);
+        }
+
+        $sprint = Sprint::from($sprintNumber);
+        $plan->setSprint($sprint);
+
+        echo 'Ingrese el enlace de GitHub: ' . PHP_EOL;
+        $gitHubLink = trim(readline());
+        $plan->setGitHubLink($gitHubLink);
+
+        echo 'Si lo desea, agregué alguna nota: ' . PHP_EOL;
+        $notes = trim(readline());
+        $plan->setNotes($notes);
+
+        if ($plan->create()) {
+            echo "¡Plan creado exitosamente!" . PHP_EOL;
+        } else {
+            echo "Error al crear el plan." . PHP_EOL;
+        }
+}
+
+function createStudyPlan(int $option)
+{
+    echo 'Por favor, ingrese la fecha de su plan en formato (dd-mm-aaaa): ' . PHP_EOL;
+    $dateInput = trim(readline());
+
+    checkDate($dateInput);
+
+    echo 'Ingrese el nombre del plan: ' . PHP_EOL;
+        $name = trim(readline());
+        $plan->setName($name);
+
+        echo 'Ingrese la ubicación del plan: ' . PHP_EOL;
+        $location = trim(readline());
+        $plan->setLocation($location);
+
+        echo 'Ingrese el tipo del plan (Restaurant, Sports, Cultural, Visit): ' . PHP_EOL;
+        $typeInput = ucfirst(trim(readline())); 
+
+        if (!in_array($typeInput, array_column(Type::cases(), 'value'))) {
+            throw new Exception("Tipo de plan no válido. Use 'Restaurant', 'Sports', 'Cultural', o 'Visit'." . PHP_EOL);
+        }
+
+        $type = Type::from($typeInput);
+        $plan->setType($type);
+
+        if ($plan->create()) {
+            echo "¡Plan creado exitosamente!" . PHP_EOL;
+        } else {
+            echo "Error al crear el plan." . PHP_EOL;
+        }
+
 }
